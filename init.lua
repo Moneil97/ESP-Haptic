e690533f1741463befc06f971a0b2b914b23b9ab
Michael O'Neil <cam8059@yahoo.com>
2019-02-13 14:24:35 -0500

    Formatting

--- init.lua
+++ init.lua
@@ -7,32 +7,37 @@ status = "OFF"
 
 server=net.createServer(net.TCP, 4000)
 server:listen(1336,function(conn) 
-    conn:on("receive",function(conn,payload)
-    i, j = string.find(payload, "control")
-
-    if i ~= nil then
-        command = string.sub(payload, i+8, j+4)
-        if command == "ON" then
-            gpio.write(outPin, gpio.HIGH)
-            status = "ON"
-        elseif command == "OFF" then
-            gpio.write(outPin, gpio.LOW)
-            status = "OFF"
-        end
-    end
-
-    conn:send('HTTP/1.1 200 OK\n\n')
-    conn:send('<!DOCTYPE HTML>\n')
-    conn:send('<html>\n')
-    conn:send('<head><meta  content="text/html; charset=utf-8">\n')
-    conn:send('<title>ESP8266 Haptic Controller</title></head>\n')
-    conn:send('<body><h1>Wifi Haptic Motor</h1>\n')
-    conn:send('<h1>Status: ' .. status .. '</h1>\n')
-    conn:send('<form action="" method="POST">\n')
-    conn:send('<input type="submit" name="control" value="ON" style="height:50px; width:50px">\n')
-    conn:send('<input type="submit" name="control" value="OFF" style="height:50px; width:50px">\n')
-    conn:send('</body></html>\n') 
+    conn:on("receive",function(client,payload)
+        i, j = string.find(payload, "control")
     
-    conn:on("sent", function(conn) conn:close() end)
-    end) 
+        if i ~= nil then
+            command = string.sub(payload, i+8, j+4)
+            if command == "ON" then
+                gpio.write(outPin, gpio.HIGH)
+                status = "ON"
+            elseif command == "OFF" then
+                gpio.write(outPin, gpio.LOW)
+                status = "OFF"
+            end
+        end
+    
+        client:send('HTTP/1.1 200 OK\n\n')
+        client:send('<!DOCTYPE HTML>\n')
+        client:send('<html>\n')
+        client:send('<head><meta  content="text/html; charset=utf-8">\n')
+        client:send('<title>ESP8266 Haptic Controller</title></head>\n')
+        client:send('<body><h1>Wifi Haptic Motor</h1>\n')
+        client:send('<h1>Status: ' .. status .. '</h1>\n')
+        client:send('<form action="" method="POST">\n')
+        client:send('<input type="submit" name="control" value="ON" style="height:50px; width:50px">\n')
+        client:send('<input type="submit" name="control" value="OFF" style="height:50px; width:50px">\n')
+        client:send('</body></html>\n') 
+
+
+        conn:on("sent", function(conn) 
+            client:close()
+            collectgarbage()
+        end)
+    end)
 end)
+
